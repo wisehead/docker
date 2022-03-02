e046cb441c0339a01baf9ea4c1ad7a4a0800bb71
wisehead <alex.chenhui@gmail.com>
2022-03-02 10:46:40 +0800

    install

---
+++ 01.install/run.sh
@@ -0,0 +1,8 @@
+#############################################################
+#   File Name: run.sh
+#     Autohor: Hui Chen (c) 2022
+#        Mail: chenhui13@xxx.com
+# Create Time: 2022/03/02-09:59:16
+#############################################################
+#!/bin/sh 
+brew install --cask --appdir=/Applications docker
